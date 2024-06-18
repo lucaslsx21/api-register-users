30da2aee6a1b8b7330cb4cc98a6f0fa020acb3e1
lucaslsx21 <git@github.com:lucaslsx21>
2024-06-18 11:38:03 -0300

    feat: fiz pequenas alterações para melhor vizualização

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -112,25 +112,26 @@ export const FormComponent = () => {
     0
   ); // Converte gorjeta para número
 
-  const calculatePaymentDatesAndValues = (numParcelas: number, totalValue: number) => {
-    const datesAndValues = [];
+  const calculatePaymentDates = (numParcelas: number) => {
+    const dates = [];
     const today = new Date();
-    const parcelValue = totalValue / numParcelas;
     for (let i = 0; i < numParcelas; i++) {
       const date = new Date(today);
       date.setMonth(today.getMonth() + i);
-      const formattedDate = date.toLocaleDateString("pt-BR", {
-        day: "2-digit",
-        month: "2-digit",
-        year: "numeric",
-      });
-      datesAndValues.push(`${formattedDate} - R$ ${parcelValue.toFixed(2)}`);
+      dates.push(
+        date.toLocaleDateString("pt-BR", {
+          day: "2-digit",
+          month: "2-digit",
+          year: "numeric",
+        })
+      );
     }
-    return datesAndValues;
+    return dates;
   };
 
   const generatePDF = () => {
     const doc = new jsPDF();
+
 
     doc.text("Relatório do Dia", 14, 22);
     doc.text(
@@ -145,7 +146,7 @@ export const FormComponent = () => {
     );
 
     autoTable(doc, {
-      head: [["Marca/Modelo", "Placa", "Serviço", "Valor", "Gorjeta", "Pago", "Método de Pagamento", "Parcelas", "Datas e Valores de Pagamento"]],
+      head: [["Marca/Modelo", "Placa", "Serviço", "Valor", "Gorjeta", "Pago", "Método de Pagamento", "Parcelas", "Datas de Pagamento",]],
       body: lista.map((item) => [
         item.marca,
         item.placa,
@@ -156,10 +157,11 @@ export const FormComponent = () => {
         item.metodoPagamento,
         item.parcelas > 1 ? `${item.parcelas}x` : "À vista",
         item.metodoPagamento === "CartãoCredito" && item.parcelas > 1
-          ? calculatePaymentDatesAndValues(item.parcelas, item.pagamento).join(", ")
+          ? calculatePaymentDates(item.parcelas).join(", ")
           : "N/A",
       ]),
       startY: 40,
+      styles: { fontSize: 7 },
     });
 
     autoTable(doc, {
@@ -228,7 +230,7 @@ export const FormComponent = () => {
               onChange={handleServicoChange}
             />
           </div>
-          <div className="flex justify-between items-center px-2 gap-3">
+          <div className="flex flex-col px-2">
             <div className="flex flex-col">
               <label className="font-semibold text-[#cd3200]">Valor:</label>
               <input
@@ -238,7 +240,8 @@ export const FormComponent = () => {
                 onChange={handlePagamentoChange}
               />
             </div>
-            <div className="flex flex-col px-2">
+
+            <div className="flex flex-col px-10">
               <label className="font-semibold text-[#cd3200]">Método de Pagamento:</label>
               <select
                 value={metodoPagamento}
@@ -251,20 +254,20 @@ export const FormComponent = () => {
                 <option value="CartãoCredito">Cartão Crédito</option>
               </select>
             </div>
-            <div className="flex flex-col">
-            <label className="font-semibold text-[#cd3200]">Parcelas:</label>
-            <select
-              className="border border-gray-300 focus:border-2 focus:border-[#cd3200] focus:outline-none bg-[#403C3D] p-2 text-zinc-100 text-bold rounded h-10 w-64"
-              value={parcelas}
-              onChange={handleParcelasChange}
-            >
-              {[...Array(12)].map((_, i) => (
-                <option key={i + 1} value={i + 1}>
-                  {i + 1}x
-                </option>
-              ))}
-            </select>
-          </div>
+            <div className="flex flex-col px-10">
+              <label className="font-semibold text-[#cd3200]">Parcelas:</label>
+              <select
+                className="border border-gray-300 focus:border-2 focus:border-[#cd3200] focus:outline-none bg-[#403C3D] p-2 text-zinc-100 text-bold rounded h-10 w-64"
+                value={parcelas}
+                onChange={handleParcelasChange}
+              >
+                {[...Array(12)].map((_, i) => (
+                  <option key={i + 1} value={i + 1}>
+                    {i + 1}x
+                  </option>
+                ))}
+              </select>
+            </div>
             <div className="flex gap-2 mr-4 mt-5">
               <label className="font-bold text-[#cd3200]">Pago</label>
               <input
@@ -293,31 +296,33 @@ export const FormComponent = () => {
           {lista.map((item, index) => (
             <div
               key={index}
-              className="grid grid-cols-3 font-semibold text-md gap-2 border-b border-gray-300 p-4 relative"
+              className="flex flex-col border-b border-gray-300 p-4"
             >
-              <p className="flex flex-col font-bold">
-                Marca:
-                <span className="font-semibold"> {item.marca}</span>
-              </p>
-              <p className="flex flex-col font-bold">
-                Placa:
-                <span className="font-semibold"> {item.placa}</span>{" "}
-              </p>
-              <p className="flex flex-col font-bold">
-                Serviço:
-                <span className="font-semibold"> {item.servico}</span>
-              </p>
-              <p className="flex flex-col font-bold">
-                Valor:
-                <span className="font-semibold"> R$ {item.pagamento}</span>
-              </p>
-              <p className="flex flex-col font-bold">
-                Método de Pagamento:
-                <span className="font-semibold"> {item.metodoPagamento}</span>
-              </p>
-              <div className="flex gap-2">
-                <div className="flex items-center gap-2">
-                  <label className="font-bold text-[#cd3200]">Gorjeta:</label>
+              <div className="flex flex-row">
+                <p className="flex flex-col font-bold mr-4">
+                  Marca:
+                  <span className="font-semibold"> {item.marca}</span>
+                </p>
+                <p className="flex flex-col font-bold mr-4">
+                  Placa:
+                  <span className="font-semibold"> {item.placa}</span>{" "}
+                </p>
+                <p className="flex flex-col font-bold">
+                  Serviço:
+                  <span className="font-semibold"> {item.servico}</span>
+                </p>
+              </div>
+              <div className="flex flex-row mt-2">
+                <p className="flex flex-col font-bold mr-4">
+                  Valor:
+                  <span className="font-semibold"> R$ {item.pagamento.toFixed(2)}</span>
+                </p>
+                <p className="flex flex-col font-bold mr-4">
+                  Método de Pagamento:
+                  <span className="font-semibold"> {item.metodoPagamento}</span>
+                </p>
+                <div className="flex flex-row">
+                  <label className="font-bold text-[#cd3200] mr-2">Gorjeta:</label>
                   <input
                     type="text"
                     value={item.gorjeta}
@@ -327,49 +332,50 @@ export const FormComponent = () => {
                     className="h-8 w-16 border border-gray-300 focus:border-2 focus:border-[#cd3200] focus:outline-none bg-[#403C3D] p-2 text-zinc-100 text-bold rounded"
                   />
                 </div>
-                <div className="flex items-center gap-2">
-                  <label className="font-bold text-[#cd3200]">Pago</label>
-                  <input
-                    type="checkbox"
-                    checked={item.foiPago}
-                    onChange={() => handleCheckboxChange(index)}
-                    className="w-6 h-6"
-                  />
-                </div>
+              </div>
+              <div className="flex flex-row mt-2">
+                <label className="font-bold text-[#cd3200] mr-2">Pago</label>
+                <input
+                  type="checkbox"
+                  checked={item.foiPago}
+                  onChange={() => handleCheckboxChange(index)}
+                  className="w-6 h-6 mr-4"
+                />
                 <button onClick={() => handleRemover(index)}>
-                  <FaRegTrashAlt className="text-red-400 absolute right-0 top-3 mt-1 mr-3" />
+                  <FaRegTrashAlt className="text-red-400" />
                 </button>
               </div>
             </div>
           ))}
-          <div className="flex flex-col justify-center items-center p-4 mb-20 font-bold mt-4">
-            <p className="text-lg">
-              Total Caixa:
-              <span className="text-[#cd3200] text-xl"> R$ {totalPagamento}</span>
-            </p>
-            <p className="text-lg">
-              Total Gorjeta:
-              <span className="text-[#cd3200] text-xl"> R$ {totalGorjeta}</span>
-            </p>
-          </div>
-          <div className="flex justify-center">
-            <button
-              onClick={generatePDF}
-              className="bg-[#cd3200] p-2 w-80 rounded-full text-white font-bold mb-20"
-            >
-              Criar Relatório do Dia
-            </button>
-          </div>
-          <div className="flex flex-col justify-center items-center my-4">
-            <button
-              onClick={handleIniciarNovoDia}
-              className="bg-green-600 p-2 w-80 rounded-full text-white font-bold mb-10"
-            >
-              Iniciar Novo Dia
-            </button>
-          </div>
         </div>
+
+        <div className="flex flex-col justify-center items-center p-4 mb-20 font-bold mt-4">
+          <p className="text-lg">
+            Total Caixa:
+            <span className="text-[#cd3200] text-xl"> R$ {totalPagamento}</span>
+          </p>
+          <p className="text-lg">
+            Total Gorjeta:
+            <span className="text-[#cd3200] text-xl"> R$ {totalGorjeta}</span>
+          </p>
+        </div>
+        <div className="flex justify-center">
+          <button
+            onClick={generatePDF}
+            className="bg-[#cd3200] p-2 w-80 rounded-full text-white font-bold mb-20"
+          >
+            Criar Relatório do Dia
+          </button>
+        </div>
+        <div className="flex flex-col justify-center items-center my-4">
+          <button
+            onClick={handleIniciarNovoDia}
+            className="bg-green-600 p-2 w-80 rounded-full text-white font-bold mb-10"
+          >
+            Iniciar Novo Dia
+          </button>
       </div>
+    </div >
     </>
   );
 };
